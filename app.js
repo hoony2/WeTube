c255e159a1aaa34a34dd80deaba35c3c878e72ce
HN <rkhoony@gmail.com>
2019-09-21 00:58:38 +0900

    1.2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
 app.get(cookieParser());
@@ -14,8 +15,8 @@ app.get(bodyParser.urlencoded({ extended: true}));
 app.get(helmet());
 app.get(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
