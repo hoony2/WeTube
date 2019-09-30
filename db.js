0064b9f42a96bf2d2ce1874a1f51fa86c2a09ee8
HN <rkhoony@gmail.com>
2019-10-01 00:40:05 +0900

    1.7

--- db.js
+++ db.js
@@ -1,50 +1,20 @@
-export const videos = [
+import  mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, 
     {
-        id:11111,
-        title: "video awesome",
-        description:  "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:111111,
-            name:"Lee",
-            email:"ch.lee@w.com",
-        }
-    },
-    {
-        id:22222,
-        title: "video super",
-        description:  "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:222222,
-            name:"Lee",
-            email:"ch.lee@w.com",
-        }
-    },
-    {
-        id:33333,
-        title: "video perfect",
-        description:  "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:333333,
-            name:"Lee",
-            email:"ch.lee@w.com",
-        }
-    },
-    {
-        id:44444,
-        title: "video good",
-        description:  "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:444444,
-            name:"Lee",
-            email:"ch.lee@w.com",
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("❤️Connected to DB");
+const handleError = () => console.log(`❌ Error on DB Connection${error}`);
+
+db.once("open", handleOpen );
+db.on("error", handleError);
