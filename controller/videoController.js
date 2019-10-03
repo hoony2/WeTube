706e5cdbb7919a45bf71b61bb281dc0dddb98141
HN <rkhoony@gmail.com>
2019-10-04 01:43:04 +0900

    3.11 Installing ESLint

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,8 @@
 import  routes  from "../routes";
 import Video from "../models/Video";
 
+ // Home
+
 export const home = async (req,res) => {
     try {
         const videos = await Video.find({}).sort({ _id: -1 });
@@ -11,12 +13,16 @@ export const home = async (req,res) => {
     }
 };
 
+ // Search
+
 export const search = (req,res) => {
     const {
         query: { term: searchingBy }
      } = req;
-    res.render("search", { pageTitle: "Search", searchingBy, videos });
+    res.render("search", { pageTitle: "Search", searchingBy});
 };
+
+ // Upload
 
 export const getUpload = (req, res) => {
     res.render("upload", { pageTitle: "Upload"});
@@ -34,6 +40,7 @@ export const postUpload = async (req, res) => {
     res.redirect(routes.videoDetail(newVideo.id));
 };
 
+ // Video Detail
 
 export const videoDetail = async (req, res) => {
     const {
@@ -41,13 +48,13 @@ export const videoDetail = async (req, res) => {
     } = req;
     try {
         const video = await Video.findById(id);
-        console.log("succeses");
     res.render("videoDetail", { pageTitle: video.title, video} );
     } catch (error) {
-        console.log("error");
         res.redirect(routes.home);
     }
 };
+
+ // Edit Video
 
 export const getEditVideo = async (req, res) =>  {
     const{
@@ -74,12 +81,16 @@ export const postEditVideo = async(req,res) =>  {
     }
 };
 
+ // Delete Video
+
 export const deleteVideo = async (req, res) =>  {
     const {
         params: { id }
     }  = req;
     try {
         await Video.findOneAndRemove({ _id: id });
-    } catch (error) {}
+    } catch (error) {
+        console.log(error);
+    }
     res.redirect(routes.home);
 };
