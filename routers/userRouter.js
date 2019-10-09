afd4c4de9e2a227c72d21b3ba5a08a4ff4488ffb
HN <rkhoony@gmail.com>
2019-10-09 21:06:37 +0900

    #6.5 MongoStore and Middlewares

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import routes from "../routes";
 import { userDetail, editProfile, changePassword } from "../controller/userController";
+import { onlyPrivate } from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
