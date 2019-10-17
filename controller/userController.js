0e6cd3053189884d8a6a54f12593f635ba87a749
HN <rkhoony@gmail.com>
2019-10-17 15:42:19 +0900

    #7.0 User Profile

--- controller/userController.js
+++ controller/userController.js
@@ -79,11 +79,25 @@ export const logout = (req, res) => {
     req.logout();
     res.redirect(routes.home);
 };
+
+export const getMe = (req,res) => {
+    res.render("userDetail", { pageTitle: "User Detail", user: req.user});;
+}
+
 export const users = (req, res) => 
     res.render("users", { pageTitle: "Users"});
-export const userDetail = (req, res) => 
-    res.render("userDetail", { pageTitle: "User Detail"});
-export const editProfile = (req, res) => 
+
+export const userDetail = async (req, res) => {
+    const { params : { id } } = req;
+    try {
+        const user = await User.findById( id ); 
+        res.render("userDetail", { pageTitle: "User Detail", user });        
+    } catch (error) {
+        res.redirect(routes.home);
+    }
+}
+
+export const getEditProfile = (req, res) => 
     res.render("editProfile", { pageTitle: "edit Profile"});
 export const changePassword = (req, res) => 
     res.render("changePassWord", { pageTitle: "Change Password"});
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword } from "../controller/userController";
+import { userDetail, getEditProfile, changePassword } from "../controller/userController";
 import { onlyPrivate } from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
