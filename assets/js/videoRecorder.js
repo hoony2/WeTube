cd7cb205273b2879993be1213162e2457d56709b
HN <rkhoony@gmail.com>
2019-10-20 14:32:27 +0900

    #10.0 API Registering a View part One

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,7 +1,7 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
-
+ 
 let streamObject;
 let videoRecorder
 
